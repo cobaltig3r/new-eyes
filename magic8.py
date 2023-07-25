0487272921165cce3caf414852aaabf01c0dd8bf
cobaltig3r <140535308+cobaltig3r@users.noreply.github.com>
2023-07-25 12:03:53 -0400

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,38 @@
+import random
+
+name = "Ben"
+question = "Will I ever learn to code?"
+answer = ""
+random_number = random.randint(1, 9)
+
+
+if random_number == 1:
+  answer = "It is decidedly so"
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5: 
+  answer = "Ask again later"
+elif random_number == 6:
+  answer = "Better not tell you now"
+elif random_number == 7: 
+  answer = "My sources say no"
+elif random_number == 8:
+  answer = "Outlook not so good"
+elif random_number == 9: 
+  answer = "Very doubtful"
+
+
+if name == "":
+  print("Question: ", question)
+elif name:
+  print(name, "asks: ", question) 
+if question == "":
+  print("No question provided")
+elif question:
+  print("Magic 8-ball's answer: ", answer)
+
+
